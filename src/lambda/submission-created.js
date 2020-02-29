aca1a1e6faab762cfa748ebad025670d245bfdf2
uberannie <ann@stax.io>
2020-03-01 00:13:50 +1100

    let's try creating a contact#

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 import fetch from 'node-fetch'
 
 const { API_TOKEN } = process.env
-const { INTERCOM_CONVERSATION_API } = process.env
+const { INTERCOM_CONVERSATION_API } = `${process.env}/conversations`
+const { INTERCOM_CONTACT_API } = `${process.env}/contacts`
+
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
@@ -10,6 +12,7 @@ exports.handler = async (event, context) => {
   }
 
   const formemail = JSON.parse(event.body).payload.email;
+  const formname = JSON.parse(event.body).payload.name;
   const email = "uberstax@stax.io"
   const intercom_conversation = {
                                   "from": {
@@ -18,9 +21,15 @@ exports.handler = async (event, context) => {
                                     },
                                   "body": `I just submitted a form for ${ formemail } from another site partyparty`
                                 }
+  const intercom_contact = {
+      	"role": "user",
+      	"email": formemail,
+      	"name": formname
+      }
 
   console.log(`Received a submission: ${formemail}`)
 
+/*
   return fetch(INTERCOM_CONVERSATION_API, {
       method: 'POST',
       headers: {
@@ -33,5 +42,18 @@ exports.handler = async (event, context) => {
     .then(data => {console.log(`we got back created_at: ${data.created_at} id: ${data.id}`)})
 
     .catch(error => ({ statusCode: 422, body: String(error) }))
+*/
+return fetch(INTERCOM_CONTACT_API, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json' },
+    body: JSON.stringify(intercom_contact) }
+    )
+  .then(response => response.json())
+  .then(data => {console.log(`we got back ${data.type} ${data.id}`)})
+
+  .catch(error => { console.log(String(error)) })
 
 }
