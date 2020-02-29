5eaeb5805381d4a0a610b5f7d7ebc632be377215
uberannie <ann@stax.io>
2020-02-29 21:50:04 +1100

    trying to connect to Intercom

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,5 +1,9 @@
-import querystring from "querystring";
+const fetch = require('node-fetch')
 
+//const { API_TOKEN } = process.env
+// let's hard code for now
+const { API_TOKEN } = "dG9rOjExZWVjYmE5XzAxMGRfNDRhOF84NDI5XzAyNjE1MzIxMjI2OToxOjA="
+const { INTERCOM_CONVERSATION_API } = 'https://api.intercom.io/conversations'
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
@@ -8,14 +12,29 @@ exports.handler = async (event, context) => {
 
   console.log(`serverless function running now`);
 
-  const email = JSON.parse(event.body).payload.email || "default email"
-  const name = JSON.parse(event.body).payload.name || "default name"
-  const message = JSON.parse(event.body).payload.message || "default message"
+  const email = JSON.parse(event.body).payload.email;
 
-  console.log(`body: Name: ${name} email: ${email} Message: ${message}`)
+  console.log(`Received a submission: ${email}`)
 
-  return {
-    statusCode: 200,
-    
-  };
-};
+  return fetch(INTERCOM_CONVERSATION_API), {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(  {
+          "from": {
+            "type": "user",
+            "id": email
+          },
+          "body": "I just submitted the contact form from the Stax website"
+        })
+    }
+    .then(response => response.json())
+    .then(data => {
+            console.log(`Submitted to Intercom:\n ${data}`)
+          })
+  .catch(error => ({ statusCode: 422, body: String(error) }))
+
+}
