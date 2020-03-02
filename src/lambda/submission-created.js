5e58b24433e7cfa00ff80c9a959ca980dddac03b
uberannie <ann@stax.io>
2020-03-02 14:11:32 +1100

    works locally let's see how server goes

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-/*import querystring from "querystring";*/
+import querystring from "query-string";
 
 const { API_TOKEN } = process.env
 const INTERCOM_CONVERSATION_API = `${process.env.INTERCOM_API}/conversations`
@@ -7,62 +7,89 @@ const INTERCOM_CONTACT_API = `${process.env.INTERCOM_API}/contacts`
 const INTERCOM_SEARCH_CONTACT_API = INTERCOM_CONTACT_API.concat("/search")
 
 exports.handler = async (event, context) => {
-  // Only allow POST
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
-  }
-
-/*
-  const params = querystring.parse(event.body);
-
-  const formemail = params.email || "no email found"
-  const formname = params.name || "no name found"
-  const formcompany = params.company || "no company found"
-
-*/
-  const formemail = JSON.parse(event.body).payload.email;
-  const formname = JSON.parse(event.body).payload.name;
-  const formcompany = JSON.parse(event.body).payload.company;
-
-  // need to find the Intercom generated ID for the contact, so we can create a conversation on behalf of that user
-  // we only have their email to work with
-  const INTERCOM_SEARCH_FIELD = "email"
-  const INTERCOM_SEARCH_OPERATOR = "="
-  const intercom_search_request = {
-     "query":  {
-        "field": INTERCOM_SEARCH_FIELD,
-        "operator": INTERCOM_SEARCH_OPERATOR,
-        "value": formemail
-      }
+    // Only allow POST
+    if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" };
     }
 
     try {
-      const response = await fetch(INTERCOM_SEARCH_CONTACT_API, {
-              method: 'POST',
-              headers: {
-                Authorization: `Bearer ${API_TOKEN}`,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'},
-              body: JSON.stringify(intercom_search_request) }
-            );
-      const data = await response.json();
 
-      if(data.errors) {
-        console.log(JSON.stringify(data))
-        throw (JSON.stringify(data))
+      const params = querystring.parse(event.body);
+      let intercom_user_id = ""
+
+      // Need the Intercom user ID
+      // obtained by creating a new user or searching for them
+      // to be able to create conversation on their behalf
+
+      // let's try to create the contact first
+      const intercom_create_contact = {
+        "role": "user",
+        "email": params.email,
+        "name": params.name,
+        "company": params.company
       }
-      // we found a match to create the conversation for
-      if(data.total_count > 0) {
-        const intercom_conversation = {
-              "from": {
-                  "type": "user",
-                  "id": data.data[0].id
-                },
-              "body": `I submitted the Contact form via stax.io with the following details\n\nName: ${ formname }\nEmail: ${ formemail }\nCompany: ${ formcompany }`
-              }
 
-        console.log(`going to create an intercom conversation now for ${ formemail }`);
+      const createcontactresponse = await fetch(INTERCOM_CONTACT_API, {
+                method: 'POST',
+                headers: {
+                  Authorization: `Bearer ${API_TOKEN}`,
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json' },
+                body: JSON.stringify(intercom_create_contact)
+              });
+
+      switch(createcontactresponse.status){
+        case 409: { // 409 returned when user already exists
+          // need to find the Intercom generated ID for the contact
+          // we only have their email to work with
+          const INTERCOM_SEARCH_FIELD = "email"
+          const INTERCOM_SEARCH_OPERATOR = "="
+          const intercom_search_request = {
+             "query":  {
+                "field": INTERCOM_SEARCH_FIELD,
+                "operator": INTERCOM_SEARCH_OPERATOR,
+                "value": params.email
+              }
+            }
+
+          const searchcontactresponse = await fetch(INTERCOM_SEARCH_CONTACT_API, {
+                  method: 'POST',
+                  headers: {
+                    Authorization: `Bearer ${API_TOKEN}`,
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'},
+                  body: JSON.stringify(intercom_search_request) }
+                );
+          const searchdata = await searchcontactresponse.json();
+
+          if(searchdata.errors || searchdata.total_count == 0) {
+            throw (JSON.stringify(searchdata))
+          }
+          else {
+            intercom_user_id = searchdata.data[0].id;
+          }
+
+          break;
+        }
+        case 200: { // successfully created new contact, thx Intercom you return the contact deets
+          const createdata = await createcontactresponse.json();
+          intercom_user_id = createdata.id;
+          break;
+        }
+        default: {  // some other problem trying to create contact
+          throw (`${ createcontactresponse.status } returned trying to create contact`)
+          break;
+        }
+      }
+
+      // create the Intercom conversation
+      const intercom_conversation = {
+            "from": {
+                "type": "user",
+                "id": intercom_user_id
+              },
+            "body": `I submitted the Contact form via stax.io with the following details\n\nName: ${ params.name }\nEmail: ${ params.email }\nCompany: ${ params.company }`
+            }
 
         const conversationresponse = await fetch(INTERCOM_CONVERSATION_API, {
             method: 'POST',
@@ -73,18 +100,11 @@ exports.handler = async (event, context) => {
             body: JSON.stringify(intercom_conversation) }
             )
           const conversationdata = await conversationresponse.json();
-          console.log(`created conversation ${conversationdata.id} for ${ formemail }`)
 
           return {
             statusCode: 200,
-            body: `created conversation ${conversationdata.id} for ${ formemail }`
+            body: `created conversation ${conversationdata.id} for ${ params.email } ${ intercom_user_id }`
           }
-        }
-        else {
-          // do we want to create a conversation anyway if we couldn't find a matching user to the email
-          console.log(`could not create conversation for ${ formemail }`)
-          throw (`could not find user ${ formemail }`)
-        }
     }
     catch (e) {
       console.log(`there was an error: ${ e }`)
